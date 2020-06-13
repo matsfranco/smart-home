cc34dbd3e8d30e3f64b00f2f2b698d7d3ab7bcd4
Mateus Franco <matsfranco@outlook.com.br>
2020-06-13 13:05:09 -0300

    DeviceManager Class included to control smart devices configuration

--- DeviceManager.py
+++ DeviceManager.py
@@ -4,6 +4,22 @@
 #   Class to implement Device Management for Smart Bulbs, sensor nodes and 
 #   another kind of connected device.
 # Module Dependencies: 
+#   - YeeLight
 # Created by: Mateus Franco @ 13/06/2020
 # Change Log:
 ############################################################################
+from yeelight import discover_bulbs
+from yeelight import Bulb
+from yeelight import LightType
+import SmartBulbManager
+
+class DeviceManager:
+    def __init__(self,smartBulbs,sensorNodes):
+        self.smartBulbs = smartBulbs
+        self.sensorNodes = sensorNodes
+
+    def fetchBulbs(self):
+        print(str(discover_bulbs()))
+
+    def fetchAvailableDevices(self):
+        self.fetchBulbs()
--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import time
 import mysql.connector
 import DatabaseManager
 import SmartBulbManager
+import DeviceManager
 # Init Database
 
 database = DatabaseManager.DatabaseConnector('localhost','smarthome','root','mateus12345')
 database.connect()
 database.createDataModel()
 
+# Discover available devices
+
+deviceManager = DeviceManager.DeviceManager('','')
+deviceManager.fetchAvailableDevices()
+
+
 # Create Bulb
-print('aqui')
-bulb = SmartBulbManager.SmartBulb('Luz do Quarto','192.168.15.14')
-bulb.turnOff()
+#print('aqui')
+#bulb = SmartBulbManager.SmartBulb('Luz do Quarto','192.168.15.14')
+#bulb.turnOff()
 database.disconnect()
